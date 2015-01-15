4c3e8629e2073b3339964beb494dda730bc1449d
Rafael Ordoñez <rafael.ordonez@medianet.es>
2015-01-15 18:46:53 +0100

    Esta clase sirve para mockear el perfil de los tests

---
+++ api-web/src/test/java/com/uma/informatica/controllers/PropertyMockingApplicationContextInitializer.java
@@ -0,0 +1,17 @@
+package com.uma.informatica.controllers;
+
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.StandardEnvironment;
+import org.springframework.mock.env.MockPropertySource;
+
+public class PropertyMockingApplicationContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+    @Override
+    public void initialize(ConfigurableApplicationContext applicationContext) {
+        MutablePropertySources propertySources = applicationContext.getEnvironment().getPropertySources();
+        MockPropertySource mockEnvVars = new MockPropertySource().withProperty("spring.profiles.active", "test");
+        propertySources.replace(StandardEnvironment.SYSTEM_ENVIRONMENT_PROPERTY_SOURCE_NAME, mockEnvVars);
+    }
+}
