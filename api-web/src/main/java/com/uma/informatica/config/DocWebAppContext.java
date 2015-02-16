4b17d6bb0e121898a0454ab06f114d11b8793725
Rafael Ordoñez <rafaelordonezvivar@gmail.com>
2015-02-16 10:12:27 +0100

    Actualizada configuración Swagger

--- api-web/src/main/java/com/uma/informatica/config/DocWebAppContext.java
+++ api-web/src/main/java/com/uma/informatica/config/DocWebAppContext.java
@@ -23,8 +23,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 @EnableWebMvc
 @EnableSwagger
+@ComponentScan(value = {"com.uma.informatica.**"})
 @EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
-@ComponentScan( { "com.uma.informatica.controllers"})
 public class DocWebAppContext extends WebMvcConfigurerAdapter {
 
 
@@ -56,59 +56,14 @@ public class DocWebAppContext extends WebMvcConfigurerAdapter {
 
     private ApiInfo apiInfo() {
         ApiInfo apiInfo = new ApiInfo(
-                "My Apps API Title",
-                "My Apps API Description",
-                "My Apps API terms of service",
-                "My Apps API Contact Email",
-                "My Apps API Licence Type",
-                "My Apps API License URL"
+                "Desarrollo de una API Restful",
+                "Página para documentar la API REST",
+                null,
+                null,
+                null,
+                null
         );
         return apiInfo;
     }
 
-    //        @Bean
-//        public InternalResourceViewResolver getInternalResourceViewResolver() {
-//            InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-////            resolver.setPrefix("/WEB-INF/pages/");
-//            resolver.setSuffix(".jsp");
-//            return resolver;
-//        }
-
-//        @Override
-//        public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-//            configurer.enable();
-//        }
-
-//        private List<HttpMessageConverter<?>> messageConverters;
-//
-//        @Override
-//        public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-//
-//            // Note: this overwrites the default message converters.
-//            converters.addAll(getMessageConverters());
-//        }
-//
-//
-//        /**
-//         * The message converters for the content types we support.
-//         *
-//         * @return the message converters; returns the same list on subsequent calls
-//         */
-//        private List<HttpMessageConverter<?>> getMessageConverters() {
-//
-//            if (messageConverters == null) {
-//                messageConverters = new ArrayList<HttpMessageConverter<?>>();
-//
-//                MappingJackson2HttpMessageConverter mappingJacksonHttpMessageConverter = new MappingJackson2HttpMessageConverter();
-//                mappingJacksonHttpMessageConverter.setObjectMapper(objectMapper());
-//                messageConverters.add(mappingJacksonHttpMessageConverter);
-//            }
-//            return messageConverters;
-//        }
-//
-//        @Bean // Also used in our integration tests.
-//        public ObjectMapper objectMapper() {
-//
-//            return new ScalaObjectMapper();
-//        }
 }
